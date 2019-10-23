0ab3a29c9dbf0ce56e4d80617d434f10451a56ca
SomePickles <55998564+SomePickles@users.noreply.github.com>
2019-10-23 13:46:13 -0700

    added a screen for buyers to accept a seller's bid submission

---
+++ app/src/main/java/com/example/yabeeprototypes/BidAccept.java
@@ -0,0 +1,13 @@
+package com.example.yabeeprototypes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BidAccept extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
