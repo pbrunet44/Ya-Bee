9a42aed2b7736ffffc38e271f4ea4d9c9e619ebc
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-10-26 16:40:57 -0700

    Established connection with Firebase!

--- app/src/main/java/com/example/yabeeprototypes/MakeListing.java
+++ app/src/main/java/com/example/yabeeprototypes/MakeListing.java
@@ -10,15 +10,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MakeListing extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_listing);
 
-        this.mDatabase = FirebaseDatabase.getInstance().getReference();
-        this.mDatabase.setValue("Boo, it's Halloween!");
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("hasta");
+
+        myRef.setValue("La Vista!");
     }
 
 }
