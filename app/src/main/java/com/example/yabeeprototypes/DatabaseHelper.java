1688813980d89305d7aa2b92029421e72e73b9ab
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-10-27 15:38:05 -0700

    Added functions in DatabaseHelper and MakeBid

--- app/src/main/java/com/example/yabeeprototypes/DatabaseHelper.java
+++ app/src/main/java/com/example/yabeeprototypes/DatabaseHelper.java
@@ -11,12 +11,35 @@ public class DatabaseHelper {
     public DatabaseHelper()
     {
         this.database = FirebaseDatabase.getInstance();
-        this.databaseReference = this.database.getReference("Posts"); // need to take into account bids...
     }
 
     public void writeNewPost(Post post)
     {
+        this.databaseReference = this.database.getReference("Posts"); // need to take into account bids...
         this.databaseReference.setValue(post);
     }
+
+    public Post readPostInformation()
+    {
+        Post post = new Post();
+        // need to work on retrieving information from the database in appropriate structure
+        // also i need to take into account as to whether i am looking for a specific post's information
+        // so i need to change the signature of this method to accept a parameter
+        return post;
+    }
+
+    public MakeBid getLowestBid()
+    {
+        // simply return lowest bid from database
+        MakeBid bid = new MakeBid();
+        return bid;
+    }
+
+    public void setLowestBid()
+    {
+        // update lowest bid in the database
+    }
+
+
 
 }
--- app/src/main/java/com/example/yabeeprototypes/MakeBid.java
+++ app/src/main/java/com/example/yabeeprototypes/MakeBid.java
@@ -12,4 +12,24 @@ public class MakeBid
         this.description = description;
         this.imageURL = imageURL;
     }
+
+    public boolean verifyBid()
+    {
+        // compare current bid to the lowest bid in the auction
+        // if it is lower, allow bid i.e. let lowest bid become current price and return true
+        // else refuse, and prompt user to enter an appropriate bid and return false;
+        return false;
+    }
+
+    public double getLowestBid()
+    {
+        return 0.0;
+    }
+
+    public void setLowestBid()
+    {
+        // sets lowest bid
+        // updates lowest bid price associated with post in database through database helper
+    }
+
 }
