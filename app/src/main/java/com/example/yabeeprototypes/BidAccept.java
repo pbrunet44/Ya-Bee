351ed6b168c9babe7b0423b418e017bf458eb5ac
SomePickles <55998564+SomePickles@users.noreply.github.com>
2019-10-28 00:46:03 -0700

    gesture controls added to BidAccept screen

--- app/src/main/java/com/example/yabeeprototypes/BidAccept.java
+++ app/src/main/java/com/example/yabeeprototypes/BidAccept.java
@@ -1,11 +1,18 @@
 package com.example.yabeeprototypes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BidAccept extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureObject;
 
     //@Override
     double dummyBid = 42.69;
@@ -28,10 +35,35 @@ public class BidAccept extends AppCompatActivity {
         askingBid.setText("Bid: $" + dummyBid);
         submissionDesc.setText(description);
 
-
+        gestureObject = new GestureDetectorCompat(this, new LearnGesture());
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
 
+    class LearnGesture extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if(e2.getX() > e1.getX()) {
+                //left to right swipe
+                Intent intense = new Intent(BidAccept.this, ItemsBuying.class);
+                Toast.makeText(getApplicationContext(), "Bid Accepted", Toast.LENGTH_LONG).show();
+                startActivity(intense);
+            }
+
+            else if(e2.getX() < e1.getX()) {
+                //right to left
+                Intent intents = new Intent(BidAccept.this, ItemsBuying.class);
+                Toast.makeText(getApplicationContext(), "Bid Declined", Toast.LENGTH_LONG).show();
+                startActivity(intents);
+
+            }
+            return true;
+        }
+    }
 
 
 
