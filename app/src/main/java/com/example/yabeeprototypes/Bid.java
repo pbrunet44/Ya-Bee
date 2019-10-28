fe89abf3c5a28a09d9439a27f2ef723aa890c84d
pbrunet44 <46230748+pbrunet44@users.noreply.github.com>
2019-10-28 10:38:46 -0700

    test commit

--- app/src/main/java/com/example/yabeeprototypes/Bid.java
+++ app/src/main/java/com/example/yabeeprototypes/Bid.java
@@ -5,7 +5,7 @@ public class Bid
     public double price = 0.00;
     public String description;
     public String imageURL;
-    //public String id; this is to ensure all bids have a unique ID
+    //public String id; this is to ensure all bids have a unique id
 
     public void setBid(double price, String description, String imageURL)
     {
