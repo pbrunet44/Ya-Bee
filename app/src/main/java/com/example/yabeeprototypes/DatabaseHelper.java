ee366500b02b9475749117ffa3db03c7debf8615
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-11-04 18:43:17 -0800

    Edited DatabaseHelper methods to reflect changes, edited comments for the methods

--- app/src/main/java/com/example/yabeeprototypes/DatabaseHelper.java
+++ app/src/main/java/com/example/yabeeprototypes/DatabaseHelper.java
@@ -22,7 +22,6 @@ public class DatabaseHelper {
         this.database = FirebaseDatabase.getInstance();
         this.databaseReference = this.database.getReference();
         this.posts = new ArrayList<>(); //Holds current state of database
-        this.databaseReference.child("Posts").addValueEventListener(postListener); //Updates current state of database
     }
 
     public void writeNewPost(Post post)
@@ -30,24 +29,10 @@ public class DatabaseHelper {
         this.databaseReference.child("Posts").child(post.getId()).setValue(post);
     }
 
-    //id the ID string of the target post
     /**
-     * Queries the database for a post with the provided ID
-     * @param
-     * @return the target Post object
+     * Retrieves all posts from the Firebase realtime database and passes it to firebaseCallback
+     * @param firebaseCallback
      */
-    /*public Post getPostByID(String id)
-    {
-        Post result = null;
-        for (Post post: this.posts) {
-            if(post.id.equals(id))
-            {
-                result = post;
-            }
-        }
-
-        return result;
-    }*/
     public void getPosts(final FirebaseCallback firebaseCallback)
     {
         this.databaseReference.child("Posts").addValueEventListener(new ValueEventListener() {
@@ -61,7 +46,6 @@ public class DatabaseHelper {
                     System.out.println(post.getTitle() +  ", " + post.getDescription());
                 }
                 firebaseCallback.onCallback(posts);
-                //System.out.println("Size of list of posts: " + posts.size());
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError)
@@ -71,6 +55,12 @@ public class DatabaseHelper {
         });
     }
 
+    /**
+     * Retrieves a specific post based on id by searching through posts
+     * @param posts
+     * @param id
+     * @return post with specific id
+     */
     public Post getPostByID(String id, List<Post> posts)
     {
         Post result = null;
@@ -86,11 +76,12 @@ public class DatabaseHelper {
     }
 
     /**
-     * Queries the database for posts with a certain category
-     * @param category the desired category (Electronics, Shoes, etc.) as a string
-     * @return an ArrayList of posts with the given category
+     * Retrieves posts from specified category
+     * @param category
+     * @param posts
+     * @return list of posts consisting of requested category
      */
-    public ArrayList<Post> getPostsByCategory(String category)
+    public ArrayList<Post> getPostsByCategory(String category, List<Post> posts)
     {
         ArrayList<Post> results = new ArrayList<>();
         for (Post post:posts) {
@@ -99,16 +90,16 @@ public class DatabaseHelper {
                 results.add(post);
             }
         }
-
         return results;
     }
 
     /**
-     * Queries the database for posts containing a certain keyword in the title
-     * @param keyword the string to be checked against post titles
-     * @return an ArrayList of posts containing the keyword in their titles
+     * Retrieves posts containing specified keyword in their titles
+     * @param keyword
+     * @param posts
+     * @return list of posts containing specified keyword in their titles
      */
-    public ArrayList<Post> getPostsByKeyword(String keyword)
+    public ArrayList<Post> getPostsByTitle(String keyword, List<Post> posts)
     {
         ArrayList<Post> results = new ArrayList<>();
         for (Post post:posts) {
@@ -122,33 +113,13 @@ public class DatabaseHelper {
     }
 
     /**
-     * Updates a post's lowest bid
+     * Updates post's lowest bid in the database
+     * @param id
+     * @param bid
      */
     public void updateLowestBid(String id, Bid bid) {
 
         this.databaseReference.child("Posts").child(id).child("lowestBid").setValue(bid);
     }
-
-    /**
-     * Scans for database updates and updates the local ArrayList, called in constructor
-     */
-    ValueEventListener postListener = new ValueEventListener() {
-        @Override
-        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            posts.clear();
-            for(DataSnapshot snapshot: dataSnapshot.getChildren())
-            {
-                Post post = snapshot.getValue(Post.class);
-                posts.add(post);
-                System.out.println(post.getTitle() +  ", " + post.getDescription());
-            }
-            //System.out.println("Size of list of posts: " + posts.size());
-        }
-        @Override
-        public void onCancelled(@NonNull DatabaseError databaseError)
-        {
-            System.out.println("ERROR: data not read!");
-        }
-    };
 
 }
