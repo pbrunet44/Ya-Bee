81cda65d90d7caa9b841b6ff01ad0f1a033d7689
Jason <jason.morgan460@myci.csuci.edu>
2019-11-12 19:39:20 -0800

    fixed a few errors from the fragmet conversion

--- app/src/main/java/com/example/yabeeprototypes/Browse.java
+++ app/src/main/java/com/example/yabeeprototypes/Browse.java
@@ -1,6 +1,7 @@
 package com.example.yabeeprototypes;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Browse extends Fragment implements PostAdapter.PostClicked{
 
@@ -16,35 +18,37 @@ public class Browse extends Fragment implements PostAdapter.PostClicked{
     RecyclerView.Adapter myAdapter;
     RecyclerView.LayoutManager layoutManager;
 
+    /*final DatabaseHelper database = new DatabaseHelper();
+    final ArrayList<Post> query = new ArrayList<>();
+    final String title = "Bike";*/
+
     ArrayList<Post> posts;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_browse_search, container, false);
 
-        /*recyclerView = findViewById(R.id.browselist);
+        recyclerView = view.findViewById(R.id.browselist);
         recyclerView.setHasFixedSize(true);
 
-        layoutManager = new LinearLayoutManager( this);
+        layoutManager = new LinearLayoutManager(this.getContext());
         recyclerView.setLayoutManager(layoutManager);
         Bid bid = new Bid(50.0, "description", "");
         posts = new ArrayList<Post>();
         posts.add(new Post("Clean Code", 28.50, "description", 2, bid, "","something", "Books", "1", new Date(), false));
         posts.add(new Post("Cracking the Coding Interview", 32, "description", 2, bid, "","something", "Books", "2", new Date(), false));
         posts.add(new Post("Java Illuminated", 124, "description", 2, bid,"","something", "Books", "3", new Date(), false));
-        posts.add(new Post("Clean Code", 28.5, "description", 2, bid, "","something", "Books", "4", new Date(), false));
+        posts.add(new Post("Clean Code", 28.50, "description", 2, bid, "","something", "Books", "4", new Date(), false));
         posts.add(new Post("Cracking the Coding Interview", 32, "description", 2, bid, "","something", "Books", "5", new Date(), false));
         posts.add(new Post("Java Illuminated", 124, "description", 2, bid,"","something", "Books", "6", new Date(), false));
         posts.add(new Post("Clean Code", 28.50, "description", 2, bid,"","something", "Books", "7", new Date(), false));
         posts.add(new Post("Cracking the Coding Interview", 32, "description", 2, bid, "","something", "Books", "8", new Date(), false));
         posts.add(new Post("Java Illuminated", 124, "description", 2, bid, "","something", "Books", "9", new Date(), false));
-        posts.add(new Post("Clean Code", 28.5, "description", 2, bid, "","something", "Books", "10", new Date(), false));
+        posts.add(new Post("Clean Code", 28.50, "description", 2, bid, "","something", "Books", "10", new Date(), false));
         posts.add(new Post("Cracking the Coding Interview", 32, "description", 2, bid ,"","something", "Books", "11", new Date(), false));
         posts.add(new Post("Java Illuminated", 124, "description", 2, bid, "","something", "Books", "12", new Date(), false));
-
-        myAdapter = new PostAdapter(this, posts);
-
-        recyclerView.setAdapter(myAdapter);*/
+        myAdapter = new PostAdapter(this.getContext(), posts);
+        recyclerView.setAdapter(myAdapter);
         return view;
     }
 
