ec818ec5057efae75bed195eb91d602a5c43240e
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-11-15 19:04:00 -0800

    Testing something

--- app/src/main/java/com/example/yabeeprototypes/Login.java
+++ app/src/main/java/com/example/yabeeprototypes/Login.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class Login extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
