65d43363f32ac53e2f588b8a80dd64cf22bf2715
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-11-15 19:09:11 -0800

    For SomePickles

---
+++ app/src/main/java/com/example/yabeeprototypes/MessagingService.java
@@ -0,0 +1,7 @@
+package com.example.yabeeprototypes;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MessagingService extends FirebaseMessagingService {
+    
+}
