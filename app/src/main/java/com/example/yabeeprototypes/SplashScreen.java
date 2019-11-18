cb86a614d04fd3ed6b71fd841878113e54599bcb
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-11-18 10:46:51 -0800

    Added a splash screen upon launch of app.

---
+++ app/src/main/java/com/example/yabeeprototypes/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.yabeeprototypes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME);
+
+
+    }
+}
