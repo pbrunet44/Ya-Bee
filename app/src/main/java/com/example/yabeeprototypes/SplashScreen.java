b36146eed208c2d34719b31bd9332678d46ef623
Kirsty <kirstyalexandra.nguegang471@myci.csuci.edu>
2019-12-08 00:57:03 -0800

    Ya-Bee logo now spins on splash screen upon startup :)

--- app/src/main/java/com/example/yabeeprototypes/SplashScreen.java
+++ app/src/main/java/com/example/yabeeprototypes/SplashScreen.java
@@ -5,25 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
-
-    private static int SPLASH_SCREEN_TIME = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
+        final ImageView bee = (ImageView) findViewById(R.id.yabeesplashscreen);
+        final Animation anim1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation anim2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.antirotate);
+
+        bee.startAnimation(anim2);
+        anim2.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void run()
-            {
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                bee.startAnimation(anim1);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        anim1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, SPLASH_SCREEN_TIME);
 
+            @Override
+            public void onAnimationRepeat(Animation animation) {
 
+            }
+        });
     }
 }
