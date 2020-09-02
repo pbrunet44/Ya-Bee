04fd247257fd90ea12573f4ca4942cf485861ce8
LAPTOP-EFOTV6HT\kirst <kirstyalexandra.nguegang471@myci.csuci.edu>
2020-09-01 22:32:44 -0700

    Created an overall layout of the new make account page, just need to figure out a color pallete.

--- app/src/main/java/com/example/yabeeprototypes/MakeAccount.java
+++ app/src/main/java/com/example/yabeeprototypes/MakeAccount.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
@@ -30,6 +32,7 @@ public class MakeAccount extends AppCompatActivity {
     private EditText username;
     private EditText name;
     private EditText email;
+    private EditText phoneNumber;
     private EditText password;
 
 
@@ -41,14 +44,14 @@ public class MakeAccount extends AppCompatActivity {
         signUpButton = (Button)findViewById(R.id.signUpButtonText);
         email = (EditText) findViewById(R.id.emailText);
         password = (EditText) findViewById(R.id.passwordText);
+        username = (EditText) findViewById(R.id.usernameText);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumber);
         name = (EditText) findViewById(R.id.nameText);
         mAuth = FirebaseAuth.getInstance();
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Here's the email I'm going to sign up with:" + email.getText().toString());
-                System.out.println("Here the password I'm going to sign up with: " + password.getText().toString());
                 createAccount(email.getText().toString(), password.getText().toString());
             }
         });
@@ -56,11 +59,9 @@ public class MakeAccount extends AppCompatActivity {
 
     // checks if user is signed in and updates UI accordingly
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        System.out.println("I am currentUser in onStart(): " + currentUser);
     }
 
     @Override
@@ -70,39 +71,28 @@ public class MakeAccount extends AppCompatActivity {
     }
 
     // create user with email, password
-    private void createAccount(final String email, String password)
-    {
-        if (validateForm()) {
+    private void createAccount(final String email, String password) {
+        if (true) {
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                // Sign in success, update appropriate information
-                                System.out.println("createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                if (user == null)
-                                    System.out.println("User is null");
-                                else {
-                                    System.out.println("User's email:" + user.getEmail());
-                                    //Register user for wishlists
-                                    final DatabaseHelper databaseHelper = new DatabaseHelper();
-                                    databaseHelper.getUsers(new UserCallback() {
-                                        @Override
-                                        public void onCallback(List<User> users) {
-                                            User user = databaseHelper.getUserByEmail(email, users);
-                                            if(user == null)
-                                            {
-                                                databaseHelper.databaseReference.child("Users").child(mAuth.getUid()).setValue(new User(email, mAuth.getUid()));
-                                            }
+                                //Register user for wishlists
+                                final DatabaseHelper databaseHelper = new DatabaseHelper();
+                                databaseHelper.getUsers(new UserCallback() {
+                                    @Override
+                                    public void onCallback(List<User> users) {
+                                        User user = databaseHelper.getUserByEmail(email, users);
+                                        if (user == null) {
+                                            databaseHelper.databaseReference.child("Users").child(mAuth.getUid()).setValue(new User(email, mAuth.getUid()));
                                         }
-                                    });
-                                }
+                                    }
+                                });
+
                                 Toast accountSuccessToast = Toast.makeText(getApplicationContext(), "Account creation successful.", Toast.LENGTH_SHORT);
                                 accountSuccessToast.show();
                                 finish();
-
-                                // updateUI(user);
                             } else {
                                 String errorMessage = "";
                                 try {
@@ -124,40 +114,32 @@ public class MakeAccount extends AppCompatActivity {
         }
     }
 
-    private boolean validateForm()
-    {
-        boolean valid = true;
-
-        String name = this.name.getText().toString();
-        if (TextUtils.isEmpty(name))
-        {
-            this.name.setError("Required.");
-            valid = false;
-        }
-        else
-            this.name.setError(null);
-
-        String email = this.email.getText().toString();
-        if (TextUtils.isEmpty(email))
-        {
-            this.email.setError("Required.");
-            valid = false;
-        }
-        else
-            this.email.setError(null);
-
-        String password = this.password.getText().toString();
-        if (TextUtils.isEmpty(password))
-        {
-            this.password.setError("Required.");
-            valid = false;
-        }
-        else
-            this.password.setError(null);
-
-        return valid;
-    }
-
+//    private boolean validateForm() {
+//        boolean valid = true;
+//
+//        String name = this.name.getText().toString();
+//        if (TextUtils.isEmpty(name)) {
+//            this.name.setError("Required.");
+//            valid = false;
+//        } else
+//            this.name.setError(null);
+//
+//        String email = this.email.getText().toString();
+//        if (TextUtils.isEmpty(email)) {
+//            this.email.setError("Required.");
+//            valid = false;
+//        } else
+//            this.email.setError(null);
+//
+//        String password = this.password.getText().toString();
+//        if (TextUtils.isEmpty(password)) {
+//            this.password.setError("Required.");
+//            valid = false;
+//        } else
+//            this.password.setError(null);
+//
+//        return valid;
+//    }
 
 
 }
