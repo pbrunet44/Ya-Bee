0a63c24be18c2f6ec5eb9ad4b50e3f28f885d669
Kirsty Alexandra Nguegang <kirstyalexandra.nguegang471@myci.csuci.edu>
2020-09-22 09:24:01 -0700

    Changed logo to be something more simple. Added a few colors to colors.xml for us to choose from. Got rid of the pervasive yellow color and made everything variations of white and gray. That can be changed later if we're feeling it

--- app/src/main/java/com/example/yabeeprototypes/SplashScreen.java
+++ app/src/main/java/com/example/yabeeprototypes/SplashScreen.java
@@ -16,45 +16,15 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        final ImageView bee = (ImageView) findViewById(R.id.yabeesplashscreen);
-        final Animation anim1 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
-        final Animation anim2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.antirotate);
-
-        bee.startAnimation(anim2);
-        anim2.setAnimationListener(new Animation.AnimationListener() {
+        final Handler handler;
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                bee.startAnimation(anim1);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-
-        anim1.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
+        },3000);
     }
 }
