6ea447b57012a3e758a3b9173340ee8e20f8322c
James Hipolito <jameshipolito39@gmail.com>
2020-11-23 17:27:12 -0800

    timer works

--- functions/index.js
+++ functions/index.js
@@ -18,15 +18,15 @@ exports.sendNotification = functions.database.ref('/Posts/{postId}/notifications
     var receivingUser = newNotification.receivingUser.uid;
     console.log(receivingUser);
     console.log(newNotification.notificationMessage);
-    
+
     var messageBody = "Tap to view.";
-    if (newNotification.typeofNotification === "WINNER" || 
-    newNotification.typeofNotification === "HAHA LOSER") {
+    if (newNotification.typeofNotification === "WINNER" ||
+        newNotification.typeofNotification === "HAHA LOSER") {
         messageBody = "Auction is over!"
     }
 
     const payLoad = {
-        notification:{
+        notification: {
             title: newNotification.notificationMessage,
             body: messageBody,
             sound: "default",
@@ -36,3 +36,56 @@ exports.sendNotification = functions.database.ref('/Posts/{postId}/notifications
 
     return admin.messaging().sendToTopic(receivingUser, payLoad);
 });
+
+exports.postTimer = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+    console.log('Message received');
+
+    return admin.database().ref('/Posts').once('value').then(snap => {
+        snap.forEach(function (child) {
+            var newPost = child.val();
+            var auctionTimeLeft = newPost.auctionTimeLeft;
+
+            console.log(newPost.id + ' auctionTimeLeft: ' + auctionTimeLeft);
+
+            if (auctionTimeLeft > 0) {
+                auctionTimeLeft = auctionTimeLeft - 300;
+            }
+            //var auctionTimeLeft = 30;
+            var auctionTimer = getAuctionTimer(auctionTimeLeft);
+            var isExpired = false;
+            if (auctionTimer === "AUCTION EXPIRED") {
+                isExpired = true;
+            }
+
+            child.ref.child('auctionTimeLeft').set(auctionTimeLeft);
+            console.log("setting " + child.val().id + " auctionTimeLeft to: " + auctionTimeLeft);
+            child.ref.child('expired').set(isExpired);
+            console.log("setting " + child.val().id + " isExpired to: " + isExpired);
+            child.ref.child('auctionTimer').set(auctionTimer);
+            console.log("setting " + child.val().id + " auctionTimer to: " + auctionTimer);
+        });
+        return console.log('post timer over');
+    });
+});
+
+function getAuctionTimer(auctionTimeLeft) {
+    if (auctionTimeLeft <= 0) {
+        return "AUCTION EXPIRED";
+    }
+    if (auctionTimeLeft > 24 * 60 * 60) {
+        return (Math.floor(auctionTimeLeft / 24 / 60 / 60) + " days, " + Math.floor((auctionTimeLeft / 60 / 60) % 24) + " hours");
+    }
+    else if (auctionTimeLeft > 60 * 60) {
+        return (Math.floor(auctionTimeLeft / 60 / 60) + " hours, " + Math.floor((auctionTimeLeft / 60) % 60) + " minutes");
+    }
+    else if (auctionTimeLeft > 60) {
+        return (Math.floor(auctionTimeLeft / 60) + " minutes, "
+            + Math.floor((auctionTimeLeft) % 60) + " seconds");
+    }
+    else {
+        return (Math.floor(auctionTimeLeft) + " seconds");
+    }
+    /*return (String.format("%02d", (this.auctionTimeLeft / 60 / 60))
+            + ":" + String.format("%02d", (this.auctionTimeLeft / 60) % 60)
+            + ":" + String.format("%02d", this.auctionTimeLeft % 60));*/
+}
